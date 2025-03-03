daeca5e9ae3983d62478c4083496aa1d73117293
Mridul Mehra <113982798+mridulmehra@users.noreply.github.com>
2025-03-03 14:32:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get("*", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
